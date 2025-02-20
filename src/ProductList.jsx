866ca878056849b8efdbd1d913425c7347ea261b
Andrew Volkov <ljubimishj@gmail.com>
2025-02-20 08:00:55 -0500

    first commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
 import './ProductList.css'
 import CartItem from './CartItem';
@@ -7,7 +7,7 @@ import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const dispatch = useDispatch();
     const plantsArray = [
         {
             category: "Air Purifying Plants",
